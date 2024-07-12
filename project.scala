277fdbb46669bff0b541024494caaa54372200a5
Bjørn Arild Kristiansen <bjorn.arild.kristiansen@finn.no>
2024-07-12 12:16:11 +0200

    init

---
+++ project.scala
@@ -0,0 +1,9 @@
+//> using scala "3"
+
+//> using dep "org.typelevel::cats-core:2.12.0"
+//> using dep "org.typelevel::cats-effect:3.5.4"
+
+//> using test.dep "org.scalameta::munit:0.7.29"
+//> using test.dep "org.typelevel::munit-cats-effect_2.0.0"
+
+//> using options "-Yimports:java.lang,scala,scala.Predef,scala.util.chaining,cats,cats.syntax.all"
